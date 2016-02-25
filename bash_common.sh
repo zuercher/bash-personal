ae2275f2e13d4f85416ce36a35d1fc221eafc57a
Stephan Zuercher <stephan@turbinelabs.io>
2016-02-25 11:13:42 -0800

    OS_INCLUDE_DIR points at /usr/include or its Xcode equivalent

--- bash_common.sh
+++ bash_common.sh
@@ -21,3 +21,23 @@ function f() {
 function fng() {
     find . -type f -a -name "$1" -print0 | xargs -0 grep "$2";
 }
+
+OS=`uname -s`
+
+# Point OS_INCLUDE_DIR at /usr/include, since I can never remember this location on the Mac
+if [ "$OS" = "Darwin" ]; then
+    MAC_SDKS="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs"
+    if [ -d "$MAC_SDKS" ]; then
+        VERSION=`ls -1 "$MAC_SDKS" | tail -n 1`
+        INCLUDE_DIR="${MAC_SDKS}/${VERSION}/usr/include"
+        if [ -d "$INCLUDE_DIR" ]; then
+            export OS_INCLUDE_DIR="$INCLUDE_DIR"
+        else
+            echo "Not setting OS_INCLUDE_DIR; $INCLUDE_DIR does not exist."
+        fi
+    else
+        echo "Not setting OS_INCLUDE_DIR; ${MAC_SDKS} does not exist. (Is Xcode installed?)"
+    fi
+else
+    export OS_INCLUDE_DIR="/usr/include"
+fi
