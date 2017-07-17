973016c900a5084177564fa2040643cc451a6163
Stephan Zuercher <stephan@turbinelabs.io>
2017-07-17 10:13:05 -0700

    handle new xcode

--- profile/set_include_dir.sh
+++ profile/set_include_dir.sh
@@ -4,8 +4,7 @@ OS=`uname -s`
 if [ "$OS" = "Darwin" ]; then
     MAC_SDKS="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs"
     if [ -d "$MAC_SDKS" ]; then
-        VERSION=`ls -1 "$MAC_SDKS" | tail -n 1`
-        INCLUDE_DIR="${MAC_SDKS}/${VERSION}/usr/include"
+        INCLUDE_DIR="${MAC_SDKS}/MacOSX.sdk/usr/include"
         if [ -d "$INCLUDE_DIR" ]; then
             export OS_INCLUDE_DIR="$INCLUDE_DIR"
         else
