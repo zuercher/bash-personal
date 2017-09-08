ee3a4cb89015b7d316fa17fef6c395147f01ef27
Stephan Zuercher <stephan@turbinelabs.io>
2017-09-07 20:43:58 -0700

    things and stuff

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -7,6 +7,7 @@ alias sum='math sum'
 alias avg='math avg'
 alias fuck='echo No, you fuck'
 
-
 alias cgi_escape="ruby -rcgi -n -e 'puts CGI.escape(\$_)'"
 alias cgi_unescape="ruby -rcgi -n -e 'puts CGI.unescape(\$_)'"
+
+alias envoy_fix_format="./ci/run_envoy_docker.sh './ci/do_ci.sh fix_format'"
