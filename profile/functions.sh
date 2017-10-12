d45465ff419f72991ac6a303d56c91917a7a836e
Stephan Zuercher <stephan@turbinelabs.io>
2017-10-12 15:15:14 -0700

    run envoy format check locally (presumes an appropriate custom-built clang-format-5.0 on the path)

--- profile/functions.sh
+++ profile/functions.sh
@@ -5,3 +5,17 @@ function f() {
 function fng() {
     find . -type f -a -name "$1" -print0 | xargs -0 grep "$2";
 }
+
+function envoy_fix_format() {
+    local ROOT=$(git rev-parse --show-toplevel)
+    if [ -z "$ROOT" ]; then
+        return 1
+    fi
+
+    if [ ! -d "$ROOT" ]; then
+        echo "not a directory: $ROOT"
+        return 1
+    fi
+
+    env BUILDIFIER="buildifier" "$ROOT"/tools/check_format.py fix "$@"
+}
