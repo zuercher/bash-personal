a301019fcafe283e178f0a881dd7b71849b574c3
Stephan Zuercher <stephan@turbinelabs.io>
2017-10-30 12:41:54 -0700

    docker clean up alias

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -10,3 +10,5 @@ alias fuck='echo No, you fuck'
 
 alias cgi_escape="ruby -rcgi -n -e 'puts CGI.escape(\$_)'"
 alias cgi_unescape="ruby -rcgi -n -e 'puts CGI.unescape(\$_)'"
+
+alias docker-clean='docker ps -a -q | xargs docker rm && docker images | grep "^<none>" | awk "{print $3}" | xargs docker rmi'
