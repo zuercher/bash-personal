ad060210be4e6d24f9928a590427e0badd88be32
Stephan Zuercher <stephan@turbinelabs.io>
2017-12-14 14:10:45 -0800

    add envoy_check_format

--- profile/functions.sh
+++ profile/functions.sh
@@ -19,3 +19,17 @@ function envoy_fix_format() {
 
     env BUILDIFIER="buildifier" "$ROOT"/tools/check_format.py fix "$@"
 }
+
+function envoy_check_format() {
+    local ROOT=$(git rev-parse --show-toplevel)
+    if [ -z "$ROOT" ]; then
+        return 1
+    fi
+
+    if [ ! -d "$ROOT" ]; then
+        echo "not a directory: $ROOT"
+        return 1
+    fi
+
+    env BUILDIFIER="buildifier" "$ROOT"/tools/check_format.py check "$@"
+}
