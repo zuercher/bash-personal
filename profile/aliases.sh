daf60b29ad7695f83ef69c67a3f9ed9ddaba1e65
Stephan Zuercher <stephan@turbinelabs.io>
2018-05-04 10:15:36 -0700

    fix docker-clean

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -11,4 +11,4 @@ alias fuck='echo No, you fuck'
 alias cgi_escape="ruby -rcgi -n -e 'puts CGI.escape(\$_)'"
 alias cgi_unescape="ruby -rcgi -n -e 'puts CGI.unescape(\$_)'"
 
-alias docker-clean='docker ps -a -q | xargs docker rm && docker images | grep "^<none>" | awk "{print $3}" | xargs docker rmi'
+alias docker-clean="docker ps -a -q | xargs docker rm && docker images | grep '^<none>' | awk '{print \$3}' | xargs docker rmi"
