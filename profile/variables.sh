07f2c9f2cf99e3b8878495eaef3813940a0115f0
Stephan Zuercher <stephan@turbinelabs.io>
2018-09-10 14:25:00 -0700

    add path to non-prefixed coreutils on mac

--- profile/variables.sh
+++ profile/variables.sh
@@ -1,6 +1,11 @@
 if $IS_MACOS; then
     # mimic dircolors defaults in Linux
     export LSCOLORS='exgxFxdxCxBxBxHbadacec'
+
+    if [ -d "/usr/local/opt/coreutils/libexec/gnubin" ]; then
+        export PATH="$PATH:/usr/local/opt/coreutils/libexec/gnubin"
+        export MANPATH="$MANPATH:/usr/local/opt/coreutils/libexec/gnuman"
+    fi
 fi
 
 # configure hh, if available
