2f78feb36802d68eaa27e90243be3d2a5cb913ca
Stephan Zuercher <stephan@turbinelabs.io>
2018-09-10 14:24:35 -0700

    envoy_fix_format: fix format on modified/staged files by default

--- profile/functions.sh
+++ profile/functions.sh
@@ -17,7 +17,22 @@ function envoy_fix_format() {
         return 1
     fi
 
-    env BUILDIFIER="buildifier" "$ROOT"/tools/check_format.py fix "$@"
+    CHECK_FMT="$ROOT"/tools/check_format.py
+    if [ ! -f "$CHECK_FMT" ]; then
+        echo "$CHECK_FMT: no such file "
+        return 1
+    fi
+
+    if [ "$#" -lt 1 ]; then
+        echo "Fixing format on all files reports by git status as added or modified:"
+        cd "$ROOT"
+        git status --porcelain=v1 | grep -E "^[MARC ][M ]" | awk '{print $NF}' | while read FILE; do
+            echo "  $FILE"
+            env BUILDIFIER="buildifier" "$CHECK_FMT" fix "$FILE"
+        done
+    else
+        env BUILDIFIER="buildifier" "$CHECK_FMT" fix "$@"
+    fi
 }
 
 function envoy_check_format() {
