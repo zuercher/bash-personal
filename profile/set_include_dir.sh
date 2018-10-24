f5d31b8db19cc6b915d9b16d3441f37204837839
Stephan Zuercher <szuercher@slack-corp.com>
2018-10-24 10:30:44 -0700

    handle non-standard xcode install path

--- profile/set_include_dir.sh
+++ profile/set_include_dir.sh
@@ -1,14 +1,23 @@
 if $IS_MACOS; then
-    MAC_SDKS="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs"
-    if [ -d "$MAC_SDKS" ]; then
-        INCLUDE_DIR="${MAC_SDKS}/MacOSX.sdk/usr/include"
-        if [ -d "$INCLUDE_DIR" ]; then
-            export OS_INCLUDE_DIR="$INCLUDE_DIR"
-        else
-            echo "Not setting OS_INCLUDE_DIR; $INCLUDE_DIR does not exist."
+    XCODE_PATH=`xcode-select -p`
+    if [ ! -d "${XCODE_PATH}" ]; then
+        XCODE_PATH="/Applications/Xcode.app"
+        if [ ! -d "${XCODE_PATH}" ]; then
+            echo "Not setting OS_INCLUDE_DIR: cannot find Xcode at ${XCODE_PATH}"
+            return
         fi
+    fi
+
+    MAC_SDKS="${XCODE_PATH}/Platforms/MacOSX.platform/Developer/SDKs"
+    if [ ! -d "${MAC_SDKS}" ]; then
+        echo "Not setting OS_INCLUDE_DIR: ${MAC_SDKS} does not exist. (Is Xcode installed?)"
     else
-        echo "Not setting OS_INCLUDE_DIR; ${MAC_SDKS} does not exist. (Is Xcode installed?)"
+        INCLUDE_DIR="${MAC_SDKS}/MacOSX.sdk/usr/include"
+        if [ -d "${INCLUDE_DIR}" ]; then
+            export OS_INCLUDE_DIR="${INCLUDE_DIR}"
+        else
+            echo "Not setting OS_INCLUDE_DIR: ${INCLUDE_DIR} does not exist."
+        fi
     fi
 else
     export OS_INCLUDE_DIR="/usr/include"
