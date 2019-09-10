272045fb5d1af63a498d4747f9d3ce8d8b96b49d
Stephan Zuercher <szuercher@slack-corp.com>
2019-09-10 16:28:38 -0700

    latest updates

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -8,7 +8,7 @@ alias sum='math sum'
 alias avg='math avg'
 alias fuck='echo No, you fuck'
 
-alias cgi_escape="ruby -rcgi -n -e 'puts CGI.escape(\$_)'"
-alias cgi_unescape="ruby -rcgi -n -e 'puts CGI.unescape(\$_)'"
+alias cgi_escape="ruby -rcgi -n -l -e 'puts CGI.escape(\$_)'"
+alias cgi_unescape="ruby -rcgi -n -l -e 'puts CGI.unescape(\$_)'"
 
 alias docker-clean="docker ps -a -q | xargs docker rm && docker images | grep '^<none>' | awk '{print \$3}' | xargs docker rmi"
