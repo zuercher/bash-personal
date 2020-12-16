ff211d88fb7ebdf80262b9bb60dfcb77c44ac5b8
Stephan Zuercher <szuercher@slack-corp.com>
2020-12-16 10:39:32 -0800

    local aliases and functions

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -12,3 +12,7 @@ alias cgi_escape="ruby -rcgi -n -l -e 'puts CGI.escape(\$_)'"
 alias cgi_unescape="ruby -rcgi -n -l -e 'puts CGI.unescape(\$_)'"
 
 alias docker-clean="docker ps -a -q | xargs docker rm && docker images | grep '^<none>' | awk '{print \$3}' | xargs docker rmi"
+
+if [ -f "${HOME}/.local_aliases.sh" ]; then
+    source "${HOME}/.local_aliases.sh"
+fi
--- profile/functions.sh
+++ profile/functions.sh
@@ -5,3 +5,7 @@ function f() {
 function fng() {
     find . -type f -a -name "$1" -print0 | xargs -0 grep "$2";
 }
+
+if [ -f "${HOME}/.local_functions.sh" ]; then
+    source "${HOME}/.local_functions.sh"
+fi
