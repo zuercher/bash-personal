5743687ac8ef86fc2f4b50ee2bc808ce3964d34b
Stephan Zuercher <stephan@sierra.ai>
2024-08-16 17:04:18 -0700

    why not 🦆

--- profile/functions.sh
+++ profile/functions.sh
@@ -1,10 +1,23 @@
 function fuck() {
     case "$1" in
-        yeah|yeah!)
+        yeah|yeah\!)
             echo "🤘"
             ;;
-        you|you!)
+        you|you\!)
             echo "Easy, turbo."
+            ;;
+        "")
+            responses=(
+                "Have you tried turning it off and back on again?"
+                "Let's not and say we did."
+                "\033[107m 🦆 \033[0m"
+                "\033[107m      \033[0m\n\033[107m🖕🕺🖕\033[0m\n\033[107m      \033[0m"
+                "Play stupid games, win stupid prizes."
+            )
+
+            size="${#responses[@]}"
+            index="$((${RANDOM} % ${size}))"
+            echo -e "${responses[$index]}"
             ;;
         *)
             echo "No, you fuck" "$@"
